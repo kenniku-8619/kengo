bbaefde589009d77b387f2290b7ea04a1dbd7c2a
kenniku-8619 <kenniku.8619@gmail.com>
2025-08-24 15:46:57 +0900

    8/24 localhostからvercel移行時のバグ修正

--- app/(auth)/bookmark/page.tsx
+++ app/(auth)/bookmark/page.tsx
@@ -3,6 +3,7 @@ import { Company } from "@/app/types/company";
 import { supabaseClient } from "@/supabase/client";
 import { useEffect, useState } from "react";
 import { deleteBookmark } from "./action";
+import Link from "next/link";
 
 export default function LoginPage(){
     const [companies, setcompanies] = useState<Company[]>([]);
@@ -20,16 +21,18 @@ export default function LoginPage(){
     return(
         <div className="flex flex-col items-center">
             <h1 className="m-3 text-3xl">ブックマークした会社一覧</h1>
-            <div className="bg-lime-300  flex justify-center py-5 flex-wrap w-100 gap-5">
+            <div className="bg-lime-300  flex justify-center my-5 py-5 flex-wrap w-100 gap-5">
                 {/* <div className="bg-orange-500 p-1"><p className="m-1">会社A</p><Link href="company/A"className="bg-gray-200 p-1 mb-2">評細を確認する</Link></div> */}
                 {companies.map((company) =>{
                     console.log(company)
                     return(
-                        <div key={company.id} className="border p-1 my-2 bg-orange-500 "> 
-                            <p>会社名:{company.name}</p>
-                            <p>郵便番号:{company.post_code}</p>
-                            <button onClick={() => deleteBookmark(company)} className="bg-gray-200 p-1 mb-2">ブックマークを削除</button>
-                        </div>
+                        <Link href={company.name}>
+                            <div key={company.id} className="border p-1 my-2 bg-orange-500 ">
+                                <p>会社名:{company.name}</p>
+                                <p>郵便番号:{company.post_code}</p>
+                                <button onClick={() => deleteBookmark(company)} className="bg-gray-200 p-1 mb-2">ブックマークを削除</button>
+                            </div>
+                        </Link>
                     )
                 })}
             </div>
--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,16 +1,17 @@
 import Link from "next/link";
 
 export default function AuthLayout({children} : {children: React.ReactNode;}) {
+    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
     return (
         <div className="bg-gray-100">
             <header className="bg-yellow-400 flex justify-evenly p-2">
-                <Link href='http://localhost:3000/home' >ホーム</Link>
+                <Link href='`${baseUrl}/home`' >ホーム</Link>
                 <div>
                     <input type="text" className="border bg-white mr-2" placeholder="検索"/>
-                    <button><Link href='http://localhost:3000/home' className="bg-gray-200 p-1 border">検索</Link></button>
+                    <button><Link href='`${baseUrl}/home`' className="bg-gray-200 p-1 border">検索</Link></button>
                 </div>
-                <Link href='http://localhost:3000/companylist'>会社一覧</Link>
-                <Link href='http://localhost:3000/user'>ユーザー情報</Link>
+                <Link href='`${baseUrl}/companylist`'>会社一覧</Link>
+                <Link href='`${baseUrl}/user`'>ユーザー情報</Link>
             </header>
              {children}
             <footer className="bg-orange-500 flex flex-col items-center p-3">
